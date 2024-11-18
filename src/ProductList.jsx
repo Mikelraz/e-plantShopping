1586a7301efd1340d5187503682869a216343083
Mikelraz <mikelrazsol@gmail.com>
2024-11-18 05:37:13 -0500

    Fix final bugs

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,5 @@
 //ProductList.jsx
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import CartSlice, { addItem } from './CartSlice';
@@ -11,6 +11,7 @@ function ProductList() {
 
     const dispatch = useDispatch();
     const cartItems = useSelector(state => state.cart.items);
+    const totalQuantity = cartItems.reduce((total, item) => total + (item.quantity || 1), 0);
 
     const plantsArray = [
         {
@@ -282,7 +283,7 @@ function ProductList() {
                     <h1 className='cart'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
                             <rect width="156" height="156" fill="none"></rect>
-                            <text className='cart_quantity_count' x="120" y="138">{ cartItems.length }</text>
+                            <text className='cart_quantity_count' x="120" y="138">{ totalQuantity }</text>
                             <circle cx="80" cy="216" r="12"></circle>
                             <circle cx="184" cy="216" r="12"></circle>
                         <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
